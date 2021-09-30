92a4bff035d69f79b1a62309fae6e8f6825087a0
Hamza Mohibe <5mohih98@solent.ac.uk>
2021-09-30 12:49:22 +0100

    Ask user for eye character

--- basics/outputs/ascii_art.py
+++ basics/outputs/ascii_art.py
@@ -4,3 +4,12 @@ print("#  o  o  #")
 print("#   --   #")
 print("##########")
 print("\t #")
+# Ask user for eye character
+print("Please enter eye character")
+eye = input()
+
+# Display an ascii art robot
+print("##########")
+print(f"#  {eye}  {eye}  #")
+print("#  ----  #")
+print("##########")
