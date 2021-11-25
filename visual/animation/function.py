bb1f95430300b6f11e92d040030fa5b1426840a8
Hamza Mohibe <5mohih98@solent.ac.uk>
2021-11-25 12:58:26 +0000

    Plot

---
+++ visual/animation/function.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+
+
+# your code here
+
+def run():
+    global fig
+    # your code here (use fig with animation function)
+
+
+run()  
